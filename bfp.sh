9788ae7fcf10529ea967b08f838ce22c4bd0cfe4
Boris Lariushin <boris@lariushin.org>
2016-09-06 23:05:49 +0300

    +rebuild

--- bfp.sh
+++ bfp.sh
@@ -1,5 +1,5 @@
 git add --all *
-git commit -m '06.09.2016 +Крысиные имена — Warrax (добавлено) +Деньги воспитывают целеустремлённых эгоистов — Е. Неймарк +Вейсман против законов природы — С.Н. Магнитов +Добавил 13 картинок в раздел "Философское и жизненное" [60] и одну в "СССР" [60]'
+git commit -m '+rebuild'
 git reflog expire --expire=now --all
 git gc --force --aggressive --prune=now
 git repack -a -d
