dfd9517eaf527ee4d420f7f126d55f59c6a68153
Boris Lariushin <boris@lariushin.org>
2016-09-14 23:50:38 +0300

    Последние добавления: 14.09.2016; Православие, национализм, либерализм, сатанизм — Warrax; Интересная черта русского характера — Warrax; Добавил 3 картинки в раздел "Социальное" [128]

--- bfp.sh
+++ bfp.sh
@@ -1,5 +1,5 @@
 git add --all *
-git commit -m '+rebuild'
+git commit -m 'Последние добавления: 14.09.2016; Православие, национализм, либерализм, сатанизм — Warrax; Интересная черта русского характера — Warrax; Добавил 3 картинки в раздел "Социальное" [128]'
 git reflog expire --expire=now --all
 git gc --force --aggressive --prune=now
 git repack -a -d
