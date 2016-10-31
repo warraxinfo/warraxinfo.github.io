8caa73d344a5dd0c06c44e5ad9153e95e1daaca2
Boris Lariushin <boris@lariushin.org>
2016-10-31 04:16:27 +0200

    Услышьте про сексуальную объективацию — Matt G. Paradise; Психопатологоанатомия VI: Je suis педофил (Джок Стерджес) — Warrax; Добавил 14 картинок в раздел "Религия: вера" [120] [131]

--- bfp.sh
+++ bfp.sh
@@ -1,5 +1,5 @@
 git add --all *
-git commit -m 'rebuild'
+git commit -m 'Услышьте про сексуальную объективацию — Matt G. Paradise; Психопатологоанатомия VI: Je suis педофил (Джок Стерджес) — Warrax; Добавил 14 картинок в раздел "Религия: вера" [120] [131]'
 git reflog expire --expire=now --all
 git gc --force --aggressive --prune=now
 git repack -a -d
