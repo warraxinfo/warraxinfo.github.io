5d6ef2232c6bcf85636044d20ce9687f50fb211e
Boris Lariushin <boris@lariushin.org>
2016-11-13 06:30:21 +0200

    11.11.2016
    Понимание ЛаВея в XXI веке, лекция LV-SB:04: Бог, которому ты поклоняешься, может быть тобой (19.10.2016)
    Рогатый салют Дню Независимости! — А.Ш. ЛаВей
    Услышьте про сексуальную объективацию (дополнен п. 20) — Matt G. Paradise
    Сталинское экономическое чудо. О Великом Архитекторе и Обществе Будущего — П. Краснов
    Добавил 20 картинок в раздел "Социальное" [140]

--- bfp.sh
+++ bfp.sh
@@ -1,5 +1,6 @@
+cd "/b/warrax.net/warraxinfo.github.io-clone/"
 git add --all *
-git commit -m 'Услышьте про сексуальную объективацию — Matt G. Paradise; Психопатологоанатомия VI: Je suis педофил (Джок Стерджес) — Warrax; Добавил 14 картинок в раздел "Религия: вера" [120] [131]'
+git commit  --file=/e/bfp.txt
 git reflog expire --expire=now --all
 git gc --force --aggressive --prune=now
 git repack -a -d
