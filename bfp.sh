8aad98d10702dbf904f2377cf4df153fd7cf1e29
Boris Lariushin <boris@lariushin.org>
2016-11-14 19:35:09 +0200

    14.11.2016
    Понятия религии и мировоззрения (с комментариями) — В. Нищий
    О потерянном рабстве и рыночной свободе — прот. А. Чаплин
    Гроздья гнева Джона Стейбека (дополнено) — El Scorpio
    Добавил 1 картинку в раздел "Сатанизм" [20]
    Добавил 20 картинок в раздел "Философское и жизненное" [100]

--- bfp.sh
+++ bfp.sh
@@ -1,6 +1,6 @@
 cd "/b/warrax.net/warraxinfo.github.io-clone/"
 git add --all *
-git commit  --file=/e/bfp.txt
+git commit --file=/e/bfp.txt
 git reflog expire --expire=now --all
 git gc --force --aggressive --prune=now
 git repack -a -d
